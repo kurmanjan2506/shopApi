c273606040c1df8fbc2453a5780a578bb203df54
kurmanjan2506 <kurmanjan25nurbekova@gmail.com>
2022-10-27 11:18:48 +0600

    added swagger

--- shopApi/urls.py
+++ shopApi/urls.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import SimpleRouter
 from category.views import CategoryViewSet
 from product.views import ProductViewSet
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Avia api test project",
+      default_version='v1',
+      description="Test REST API backend at django",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = SimpleRouter()
 router.register('categories', CategoryViewSet)
 router.register('products', ProductViewSet)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api/v1/accounts/', include('account.urls')),
